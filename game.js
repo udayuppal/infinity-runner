cece7e0b4a79bddc137782f0cd6e45a00b232e52
Uday Uppal <uday.uppal.3@gmail.com>
2016-07-28 19:09:59 -0700

    "added canvas file"

---
+++ game.js
@@ -0,0 +1,29 @@
+//Constants
+var CANVAS = document.getElementById("game");
+var CONTEXT = canvas.getContext("2d");
+var FPS = 100;
+
+//Colors and Text
+
+
+//Variables
+
+
+//updates and calculations
+function updateElements() {
+
+}
+
+//drawing all elements
+function drawElements() {
+
+}
+
+//main function running based on FPS
+function main() {
+    console.log("ello");
+    updateElements();
+    drawElements();
+
+    window.setTimeout(1000/FPS, function() {main()})
+}
