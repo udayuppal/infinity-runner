ed96fd8cf889e8359e6d9b74158c8b2aaca0b000
Uday Uppal <uday.uppal.3@gmail.com>
2016-07-30 00:05:22 -0700

    draw

--- game.js
+++ game.js
@@ -32,10 +32,9 @@ window.onload = function () {
         this.size = RUNNER_SIZE;
         this.draw = function () {
             var sprite = new Image();
-            sprite.scr = "sprites/Runner.png"
+            sprite.src = "sprites/Runner.png"
             //var frameToDraw = counter % 4;
             //CONTEXT.drawImage(sprite, 0, 32*frameToDraw, 32, 32, 50, 50, 32, 32);
-            console.log(sprite);
             CONTEXT.drawImage(sprite, 10, 10, 32, 128);
         }
     }
